914c964f9ea6266aa6fda824435e0e38906fd382
cqrlosdiqz <carlosdf75@gmail.com>
2021-10-07 20:55:18 +0200

    api rest mongodb

--- router/characters.routes.js
+++ router/characters.routes.js
@@ -1,42 +1,139 @@
 const { Router } = require('express');
 const router = Router();
+const { MongoClient } = require('mongodb');
 
-const { characters } = require('../data/characters');
+const URI = process.env.MONGODB_URI;
+const client = new MongoClient(URI);
 
-router.get('/', (req, res) => {
-  const data = characters.getCharacter();
-  res.send(data);
+//Get all characters
+router.get('/', async (req, res) => {
+  try {
+    const database = await client.connect();
+    const result = await database
+      .db('characters_db')
+      .collection('characters')
+      .find({}, { projection: { _id: 0 } })
+      .toArray();
+
+    res.send(result);
+  } catch (err) {
+    console.error(err);
+  } finally {
+    await client.close();
+  }
 });
 
-router.get('/name/:name', (req, res) => {
+//Get character by name
+router.get('/name/:name', async (req, res) => {
   const { name } = req.params;
-  const data = characters.getCharacterByName(name);
-  res.send(data);
+  try {
+    const database = await client.connect();
+    const result = await database
+      .db('characters_db')
+      .collection('characters')
+      .findOne({ username: name }, { projection: { _id: 0 } });
+
+    res.send(result);
+  } catch (err) {
+    console.error(err);
+  } finally {
+    await client.close();
+  }
 });
 
-router.get('/profile/:profile', (req, res) => {
+//Get characters by profile
+router.get('/profile/:profile', async (req, res) => {
   const { profile } = req.params;
-  const data = characters.getCharactersByProfile(profile);
-  res.send(data);
+
+  try {
+    const database = await client.connect();
+    const result = await database
+      .db('characters_db')
+      .collection('characters')
+      .find({ role: profile }, { projection: { _id: 0 } })
+      .toArray();
+
+    res.send(result);
+  } catch (err) {
+    console.error(err);
+  } finally {
+    await client.close();
+  }
 });
 
-router.post('/new', (req, res) => {
+router.post('/new', async (req, res) => {
   const { id, username, role } = req.body;
-  characters.addNewCharacter({ id: parseInt(id), username, role });
-  res.send('Saved');
+  const doc = {
+    id: parseInt(id),
+    username,
+    role,
+  };
+
+  try {
+    const database = await client.connect();
+    const result = await database
+      .db('characters_db')
+      .collection('characters')
+      .insertOne(doc);
+
+    res.send(`A document was inserted with the _id: ${result.insertedId}`);
+  } catch (err) {
+    console.error(err);
+  } finally {
+    await client.close();
+  }
 });
 
-router.delete('/delete/:id', (req, res) => {
+router.delete('/delete/:id', async (req, res) => {
   const { id } = req.params;
-  characters.removeCharacter(parseInt(id));
-  res.send('Deleted');
+
+  try {
+    const database = await client.connect();
+
+    const result = await database
+      .db('characters_db')
+      .collection('characters')
+      .deleteOne({ id: parseInt(id) });
+    if (result.deletedCount === 1) {
+      res.send('Successfully deleted one document.');
+    } else {
+      res.send('No documents matched the query. Deleted 0 documents.');
+    }
+  } catch (err) {
+    console.error(err);
+  } finally {
+    await client.close();
+  }
 });
 
-router.put('/update/:id', (req, res) => {
+router.put('/update/:id', async (req, res) => {
   const { id } = req.params;
-  const dataUpdate = { id: parseInt(id), ...req.body };
-  characters.updateCharacter(dataUpdate);
-  res.send('Updated');
+  const { username, role } = req.body;
+
+  try {
+    const database = await client.connect();
+
+    const filter = { id: parseInt(id) };
+    const options = { upsert: true };
+    const updateDoc = {
+      $set: {
+        username:!!username && username,
+        role:!!role && role,
+      },
+    };
+
+    const result = await database
+      .db('characters_db')
+      .collection('characters')
+      .updateOne(filter, updateDoc, options);
+    res.send(
+      `${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`
+    );
+  } catch (err) {
+    console.error(err);
+  } finally {
+    await client.close();
+  }
 });
 
 module.exports = router;
